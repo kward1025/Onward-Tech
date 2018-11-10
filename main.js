d95a55473a99d8724496e26de85598d17649d4dd
Shariq Torres <shariq.torres@gmail.com>
2018-11-10 14:33:57 -0800

    changes

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(function(){
- var listitems=$("#Nav_Category ul li" );  
- console.log(listitems);
+var selectedCategory = $("#Nav_Category ul li.selected").html();
+var selectedDecade = 1990;
+console.log("the default category is " + selectedCategory);
+ $("#Nav_Category ul li" ).on("click", function(e){
+    $("#Nav_Category ul li.selected").removeClass("selected") 
+    $(e.target).addClass("selected");
+    selectedCategory = $(e.target).html();
+    console.log(selectedCategory);
+ })
+
+ $("#Timeline .bar .bubbles").on("click", function(e){
+    console.log(e.target);
+    selectedDecade = $(e.target).html();
+    console.log(selectedDecade);
+ })
 })
+
