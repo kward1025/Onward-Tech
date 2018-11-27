eccb2b671157712900e38b9e29321336aedab7b9
Shariq Torres <shariq.torres@gmail.com>
2018-11-27 02:47:02 -0500

    added the home page

--- wokanda/settings.py
+++ wokanda/settings.py
@@ -122,4 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, "wokanda", "static")
+]
+
 django_heroku.settings(locals())
