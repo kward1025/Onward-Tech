dbb2d671d05d9804155135cd020737087d6cce26
Shariq Torres <shariq.torres@gmail.com>
2018-12-01 13:19:15 -0800

    query

--- wokanda/core/views.py
+++ wokanda/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from wokanda.core.models import Nominee, Category
 # Create your views here.
 def home(request):
     return render(request, "index.html", {
@@ -14,9 +14,17 @@ def about(request):
     })
 
 def category(request):
+    def chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+    
+    selected_category = Category.objects.get(name="Film")
+    selected_nominees = Nominee.objects.filter(category=selected_category, year_made__gte=1990, year_made__lte=1999)
+    nominees_chunked = list(chunks(selected_nominees, 3))
     return render(request, "category.html", {
         "title": "Wokanda Awards - Categories",
-        "selected": "category"
+        "selected": "category",
+        "nominee_list": nominees_chunked
     })
 
 def nominate(request):
