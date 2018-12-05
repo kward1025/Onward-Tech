98e6083000b5f7b90b17af93a6d91052bb90a1c9
Shariq Torres <shariq.torres@gmail.com>
2018-12-04 23:50:32 -0800

    changed ordering

--- wokanda/core/admin.py
+++ wokanda/core/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Category, Nominee
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Nominee)
+
+@admin.register(Nominee)
+class NomineeAdmin(admin.ModelAdmin):
+    def get_ordering(self, request):
+        return ['category', 'year_made']
+        
