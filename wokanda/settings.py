22c2b885704a9fce8c20c3d1a25db0810dd6a191
Shariq Torres <shariq.torres@gmail.com>
2018-12-04 23:31:03 -0800

    settings

--- wokanda/settings.py
+++ wokanda/settings.py
@@ -139,5 +139,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-
+AWS_STORAGE_BUCKET_NAME = 'wokanda'
 django_heroku.settings(locals())
