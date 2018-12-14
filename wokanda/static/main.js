783debdef31025600b2863468edf760af9fda490
Shariq Torres <shariq.torres@gmail.com>
2018-12-14 03:27:17 -0800

    additions

--- wokanda/static/main.js
+++ wokanda/static/main.js
@@ -1,26 +1,33 @@
 $(function(){
    var selectedCategory = $("#Nav_Category ul li.selected").html();
    var selectedDecade = 1990;
+   var first_load = true;
    console.log("the default category is " + selectedCategory);
    $("#Nav_Category ul li" ).on("click", function(e){
       $("#Nav_Category ul li.selected").removeClass("selected") 
       $(e.target).addClass("selected");
       selectedCategory = $(e.target).html();
-      console.log(selectedCategory);
+      first_load = false;
    })
 
    $("#Timeline .bar .bubbles").on("click", function(e){
       console.log(e.target);
       selectedDecade = $(e.target).html();
-      console.log(selectedDecade);
+      $("#Timeline .bar .bubbles_selected").removeClass("bubbles_selected");
+      $(e.target).addClass("bubbles_selected");
+      first_load = false;
    })
 
-   $.get("/query_nominees", 
-         {category: selectedCategory, decade: selectedDecade}
-      ).then((res) => {
-         console.log(res);
-      }, (err) => {
-         console.log(err);
+   if(!first_load){
+      console.log(selectedCategory);
+      console.log(selectedDecade);
+      $.get("/query_nominees", 
+            {category: selectedCategory, decade: selectedDecade}
+         ).then((res) => {
+            console.log(res);
+         }, (err) => {
+            console.log(err);
       });
-})
+   }
+});
 
