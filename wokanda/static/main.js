0b5215a0ff314ff3556b924d80dae8597e07302c
Shariq Torres <shariq.torres@gmail.com>
2018-12-23 16:05:03 -0800

    sds

--- wokanda/static/main.js
+++ wokanda/static/main.js
@@ -1,7 +1,44 @@
 $(function(){
-   var selectedCategory = $("#Nav_Category ul li.selected").html();
-   var selectedDecade = 1990;
-   var first_load = true;
+   var selectedCategory, selectedDecade, first_load;
+   if(window.location.search != ""){
+      var search = window.location.search.substr(1, window.location.search.length);
+      var search_split = search.split("&");
+      var keys = {};
+      for( var i = 0; i < search_split.length; i++){
+         var parts = search_split[i].split("=");
+         keys[parts[0]] = parts[1];
+      }
+
+      if('category' in keys && 'decade' in keys){
+         var menus = $("#Nav_Category ul li.selected");
+         for(var i = 0; i < menus.length; i++){
+            if(menus[i].innerText == keys['category']){
+               $("#Nav_Category ul li.selected").removeClass("selected");
+               $(menus[i]).addClass("selected");
+            }
+         }
+
+         var times = $("#Timeline .bar .bubbles");
+         for(var i =0; i < times.length; i++){
+            if(times[i].innerText.search(keys['decade']) == 0){
+               var prev = $("#Timeline .bar .bubbles_selected").removeClass("bubbles_selected");
+               prev.addClass("bubbles");
+               $(times[i]).addClass("bubbles_selected");
+            }
+         }
+      }
+
+      selectedCategory = keys['category'];
+      selectedDecade = keys['decade'];
+      first_load = false;
+      getNominees(first_load);
+   }else{
+   
+      selectedCategory = $("#Nav_Category ul li.selected").html();
+      selectedDecade = 1990;
+      first_load = true;
+
+   }
    console.log("the default category is " + selectedCategory);
    $("#Nav_Category ul li" ).on("click", function(e){
       $("#Nav_Category ul li.selected").removeClass("selected") 
